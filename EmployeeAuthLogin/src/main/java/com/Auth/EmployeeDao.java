e44c1dc928f4e2513795ccfc7e27c6bcb6fe2e0b
Madhuchhanda-123 <84094176+Madhuchhanda-123@users.noreply.github.com>
2021-05-14 22:20:35 +0530

    Add files via upload

---
+++ EmployeeAuthLogin/src/main/java/com/Auth/EmployeeDao.java
@@ -0,0 +1,12 @@
+package com.auth.dao;
+
+import java.util.List;
+
+import com.auth.model.Employee;
+
+public interface EmployeeDao {
+	void insertEmployee(Employee cus);
+	void insertEmployees(List<Employee> employees);
+	List<Employee> getAllEmployees();
+	Employee getEmployeeById(String empId);
+}
